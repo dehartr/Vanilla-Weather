57ddd2d9721632eb9d4f1c89889acf3d1d09caca
dehartr <rasheemadehart@gmail.com>
2023-01-24 22:44:44 -0500

    update date & time

--- js/index.js
+++ js/index.js
@@ -1,15 +1,39 @@
+function formatDate(timestamp){
+    //calculate the date
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if(hours < 10){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if(minutes< 10 ){
+        minutes = `0${minutes};`
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date. getDay()];
+    return `${day} ${hours}: ${minutes}`
+}
+
+
 function displayTemp(response){
-    console.log(response)
+    
     let tempElement = document.querySelector("#temperature")
     tempElement.innerHTML = Math.round(response.data.temperature.current);
+    
     let cityElement = document.querySelector('#city')
     cityElement.innerHTML = response.data.city;
+    
     let description = document.querySelector('#description')
     description.innerHTML = response.data.condition.description;
+    
     let humidity = document.querySelector("#humidity")
     humidity.innerHTML = response.data.temperature.humidity;
+    
     let wind = document.querySelector('#wind')
     wind.innerHTML = Math.round(response.data.wind.speed);
+    
+    let date = document.querySelector('#date') 
+    date.innerHTML = formatDate(response.data.time * 1000)
 
 }
 
