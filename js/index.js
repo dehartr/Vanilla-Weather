0b9962569b54d1954499892b47e45ad620ddc523
dehartr <rasheemadehart@gmail.com>
2023-01-25 21:00:08 -0500

    added fahrenheit to celsius conversion

--- js/index.js
+++ js/index.js
@@ -36,11 +36,14 @@ function displayTemp(response){
     let date = document.querySelector('#date') 
     date.innerHTML = formatDate(response.data.time * 1000)
 
+    fahrenheitTemp = response.data.temperature.current;
 
     let icon = document.querySelector("#icon")
     icon.setAttribute(
   "src",
   `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+
+  
 );
 
 }
@@ -59,7 +62,36 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function displaycelsiusTemperature(event){
+  event.preventDefault();
+  let tempElement = document.querySelector("#temperature");
+
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+ let celsiusTemperature = (fahrenheitTemp - 32) / 1.8;
+ tempElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+function displayfahrenheitTemperature(event){
+ event.preventDefault();
+ let tempElement = document.querySelector("#temperature");
+
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+ tempElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let fahrenheitTemp = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displaycelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayfahrenheitTemperature);
 
 search("Atlanta");
